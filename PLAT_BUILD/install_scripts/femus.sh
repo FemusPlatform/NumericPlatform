ff9bc69c1485ad9056095eddccda45d154ffcf4f
roberto.davia <roberto.davia2@unibo.it>
2018-12-12 10:23:16 +0100

    added 2d and 3d library compilation during femus installation

--- PLAT_BUILD/install_scripts/femus.sh
+++ PLAT_BUILD/install_scripts/femus.sh
@@ -19,7 +19,7 @@ source plat_conf.sh     # set up environment
 
 echo $SCRIPT_NAME ": 1c Platform set up:  "
 echo $SCRIPT_NAME ": 1c BUILD dir (BUILD_DIR) is                           = " $BUILD_DIR
-echo $SCRIPT_NAME ": 1c INSTALLATION platform DIR (INSTALL_DIR) is         = " $PLAT_CODES_DIR/femus_repo
+echo $SCRIPT_NAME ": 1c INSTALLATION platform DIR (INSTALL_DIR) is         = " $PLAT_CODES_DIR/femus
 echo $SCRIPT_NAME ": 1c INSTALL_PLAT_LOG_DIR  (log dir)                    = " $INSTALL_PLAT_LOG_DIR
 echo $SCRIPT_NAME ": 1c INSTALL_PLAT_PKG_DIR  (application dir)            = " $INSTALL_PLAT_PKG_DIR
 echo $SCRIPT_NAME ": Script set up for "  $SCRIPT_NAME ": 1 -> 1a. base dir -> 1b. make dir -> 1c. setup " 
@@ -27,13 +27,29 @@ echo $SCRIPT_NAME ": Script set up for "  $SCRIPT_NAME ": 1 -> 1a. base dir -> 1
 cd $PLAT_CODES_DIR
 
 # GET PACKAGE SOURCE FILES ===================================================
-  if [ ! -d $PLAT_CODES_DIR/femus_repo ]; then
+  if [ ! -d $PLAT_CODES_DIR/femus ]; then
     git clone https://github.com/FemusPlatform/femus.git
   else
     echo
-    echo "Directory $PLAT_CODES_DIR/femus_repo already exists"
+    echo "Directory $PLAT_CODES_DIR/femus already exists"
     echo "To perform a fresh installation of FEMuS please remove the older directory"
     echo
   fi
+  
+  echo "Setting environment for femus "
+  cd $PLAT_CODES_DIR/femus
+  source femus.sh
+  
+  echo "Compiling femus library for 2D geometry "
+  cd $PLAT_CODES_DIR/femus/applications/lib_femus2D
+  configure_application opt
+  make src_clean
+  make 
+  
+  echo "Compiling femus library for 3D geometry "
+  cd $PLAT_CODES_DIR/femus/applications/lib_femus3D
+  configure_application opt
+  make src_clean
+  make
 
 cd $BUILD_DIR
