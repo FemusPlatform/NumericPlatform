9fae9bb96fffa82b007a16f1641b05e9eae603c1
Antonio Cervone <ant.cervone@gmail.com>
2023-08-11 16:53:35 +0200

    setup third party dependencies with spack

---
+++ spack_setup.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# exit on errors
+set -e
+
+# check that spack is available
+if ! command -v spack &> /dev/null
+then
+    echo "spack could not be found, please check your installation"
+    exit 1
+fi
+
+# install the spack environment
+spack env activate spack_env
+spack install
+
+# link packages in PLAT_THIRD_PARTY
+mkdir -p PLAT_THIRD_PARTY
+
+hdf5_dir=$(spack location -i hdf5)
+echo "linking hdf5 from ${hdf5_dir}"
+ln -s ${hdf5_dir} PLAT_THIRD_PARTY/hdf5
+
+med_dir=$(spack location -i salome-med)
+echo "linking med from ${med_dir}"
+ln -s ${med_dir} PLAT_THIRD_PARTY/med
+
+medcoupling_dir=$(spack location -i salome-medcoupling)
+echo "linking medcoupling from ${medcoupling_dir}"
+ln -s ${medcoupling_dir} PLAT_THIRD_PARTY/medcoupling
+
+openmpi_dir=$(spack location -i openmpi)
+echo "linking openmpi from ${openmpi_dir}"
+ln -s ${openmpi_dir} PLAT_THIRD_PARTY/openmpi
+
+petsc_dir=$(spack location -i petsc)
+echo "linking medcoupling from ${petsc_dir}"
+ln -s ${petsc_dir} PLAT_THIRD_PARTY/petsc
